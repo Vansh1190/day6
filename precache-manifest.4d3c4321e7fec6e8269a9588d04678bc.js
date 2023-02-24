952241db914c2fae5722c24caababc2582785c78
Vansh <vanshk605@gmail.com>
2023-02-24 20:53:22 +0530

    Updates

---
+++ precache-manifest.4d3c4321e7fec6e8269a9588d04678bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c6534f7edb5da40ddf4",
+    "url": "/day6/static/css/main.5e505aa5.chunk.css"
+  },
+  {
+    "revision": "4c6534f7edb5da40ddf4",
+    "url": "/day6/static/js/main.4c6534f7.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/day6/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "bbd20b2f519f47ed7c33",
+    "url": "/day6/static/js/2.bbd20b2f.chunk.js"
+  },
+  {
+    "revision": "96aecbbc05381d4fc445",
+    "url": "/day6/static/js/runtime~main.96aecbbc.js"
+  },
+  {
+    "revision": "02392b7ed583fcae6b3a74867cfa5c0f",
+    "url": "/day6/index.html"
+  }
+];
